e4e3de67ed2eeba5e1cf250eaff4f6a71b78cc40
Nihar Raote <NAPOLEON039@users.noreply.github.com>
2017-12-03 18:35:15 +0530

    Added files

---
+++ resp.js
@@ -0,0 +1,5 @@
+$("document").ready(function(){
+$("button").addClass("color_shade");		
+$(".color_shade").css("background","linear-gradient(45deg,#fe6b8b 30%, #ff8e53 90%)");				  
+$("button").css("color","white");
+});
